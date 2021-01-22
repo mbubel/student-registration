02433f94d20393d65441c6317ef1a3c634f4d376
Christina2021 <65867599+Christina2021@users.noreply.github.com>
2021-01-21 20:15:43 -0800

    Updated seed file

--- db/seed.sql
+++ db/seed.sql
@@ -5,11 +5,11 @@ VALUES  ('La Mesa', '619-857-1270', 'lamesa@artwithlarisse.com'),
         ('Santee', '619-855-2787', 'santee@artwithlarisse.com'),
         ('Bonita', '619-508-1299', 'bonita@artwithlarisse.com');
 
-INSERT INTO students (last_name, first_name, date_of_birth, student_status, createdAt, updatedAt, studio_id)
-VALUES  ('Johnson', 'Suzy', '2009-10-11', 'Intro', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 1),
-        ('Simpson', 'Johnny', '2014-02-14', 'Current', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 2),
-        ('Poppy', 'Wesley', '2012-09-02', 'Current', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 3),
-        ('Wilson', 'Cindy', '2011-08-11', 'Done', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 1);
+INSERT INTO students (last_name, first_name, date_of_birth, student_status, createdAt, updatedAt, studio_id, class_id)
+VALUES  ('Johnson', 'Suzy', '2009-10-11', 'Intro', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 1, 1),
+        ('Simpson', 'Johnny', '2014-02-14', 'Current', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 2, 2),
+        ('Poppy', 'Wesley', '2012-09-02', 'Current', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 3, 3),
+        ('Wilson', 'Cindy', '2011-08-11', 'Done', '1000-01-01 00:00:00', '1000-01-01 00:00:00', 1, 1);
         
 INSERT INTO availableclasses (day_of_week, start_time, end_time, teacher, createdAt, updatedAt, studio_id)
 VALUES  ('Monday', '2:00PM', '3:30PM', 'Ms. Skye Jollie','1000-01-01 00:00:00', '1000-01-01 00:00:00',1),
